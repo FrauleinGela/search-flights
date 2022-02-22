2d8894aeb74285ee22e3745e749c11449af5ea67
Angela <angefraugela@gmail.com>
2022-02-22 21:03:10 +0100

    core.module - Missing adding interceptor

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AuthService } from './auth/auth.service';
+import { TokenInterceptor } from './auth/token.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
@@ -12,6 +14,7 @@ import { AuthService } from './auth/auth.service';
   ],
   providers: [
     AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
   ],
   exports: []
 })
